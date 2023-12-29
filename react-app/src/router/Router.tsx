8aa43ab15cfb112ee5617e946d7d2ca050b591a3
nizidara <contact@nizidara.com>
2023-12-29 13:21:42 +0900

    create Router

---
+++ react-app/src/router/Router.tsx
@@ -0,0 +1,14 @@
+import { FC, memo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "../components/pages/Home";
+import { Page404 } from "../components/pages/Page404";
+export const Router: FC = memo(() => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="*" element={<Page404 />} />
+            </Routes>
+        </BrowserRouter>
+    )
+})
