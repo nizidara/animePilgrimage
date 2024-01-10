53f206125ee709402cbe175122b2a0d1c501271c
nizidara <contact@nizidara.com>
2024-01-10 22:48:08 +0900

    install bootstrap and create Header

--- react-app/src/components/organisms/layout/Header.tsx
+++ react-app/src/components/organisms/layout/Header.tsx
@@ -1,6 +1,35 @@
-import { FC, memo } from "react"
+import { FC, memo, useCallback } from "react"
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 
 export const Header: FC = memo(() => {
-    return <div style={{height: "50px", backgroundColor: "teal"}}></div>
+    const navigate = useNavigate();
+
+    const onClickHome = useCallback(() => navigate("/home"), [navigate]);
+    const onClickMap = useCallback(() => navigate("/map"), [navigate]);
+    const onClickSearch = useCallback(() => navigate("/search"), [navigate]);
+    const onClickRegister = useCallback(() => navigate("/register_place"), [navigate]);
+    const onClickGuide = useCallback(() => navigate("/guide"), [navigate]);
+
+
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="./home">にじげんたび</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link onClick={onClickHome}>Home</Nav.Link>
+                            <Nav.Link onClick={onClickMap}>Map</Nav.Link>
+                            <Nav.Link onClick={onClickSearch}>検索</Nav.Link>
+                            <Nav.Link onClick={onClickRegister}>聖地登録</Nav.Link>
+                            <Nav.Link onClick={onClickGuide}>利用ガイド</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
 });
