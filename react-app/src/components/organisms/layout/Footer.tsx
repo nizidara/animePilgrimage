c8c06de125ed1f4be54304cbbbec9b8ee02196ed
nizidara <contact@nizidara.com>
2024-01-14 14:13:34 +0900

    fix footer

--- react-app/src/components/organisms/layout/Footer.tsx
+++ react-app/src/components/organisms/layout/Footer.tsx
@@ -4,8 +4,8 @@ import { FC, memo } from "react"
 export const Footer: FC = memo(() => {
 
     return (
-        <>
-            <p>フッターです．</p>
-        </>
+        <div className="mt-aoto py-2 text-center fixed-bottom">
+            <p>Copyright&copy; にじだら, {new Date().getFullYear()} All Rights Reserved.</p>
+        </div>
     )
 });
