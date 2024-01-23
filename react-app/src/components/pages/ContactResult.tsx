5d0617f98ce428b187b016a11c23044d1c026dee
nizidara <contact@nizidara.com>
2024-01-23 22:31:56 +0900

    execute contact API in front

--- react-app/src/components/pages/ContactResult.tsx
+++ react-app/src/components/pages/ContactResult.tsx
@@ -1,20 +1,21 @@
 import {memo, FC, useState} from "react";
 import { Container } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
-import { sendContents } from "../../hooks/useSendContact";
+import { responseSendContents } from "../../hooks/useSendContact";
 
 export const ContactResult: FC = memo(() =>{
     const location = useLocation();
     
-    const sendContents = location.state.sendContents as sendContents;
+    const responseSendContents = location.state.responseSendContents as responseSendContents;
 
     return (
         <Container>
             <h2>お問い合わせ完了ページです．</h2>
-            <p>名前：{sendContents.name}さん</p>
-            <p>メール：{sendContents.email}</p>
-            <p>タイトル：{sendContents.title}</p>
-            <p>内容：{sendContents.contents}</p>
+            <p>id:{responseSendContents.id}</p>
+            <p>名前：{responseSendContents.name}さん</p>
+            <p>メール：{responseSendContents.email}</p>
+            <p>タイトル：{responseSendContents.title}</p>
+            <p>内容：{responseSendContents.contents}</p>
         </Container>
     )
 });
--- react-app/src/hooks/useSendContact.ts
+++ react-app/src/hooks/useSendContact.ts
@@ -1,4 +1,5 @@
-import { useCallback } from "react";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 export type sendContents ={
@@ -8,11 +9,34 @@ export type sendContents ={
     contents: string;
 }
 
+export type responseSendContents ={
+    id: number;
+    name: string;
+    email: string;
+    title: string;
+    contents: string;
+}
+
 export const useSendContact = () => {
+    const [responseSendContents, setResponseSendContents] = useState<responseSendContents | null>(null);
     const navigation = useNavigate();
+    const url = "http://127.0.0.1:8000";
 
     const send = useCallback((sendContents : sendContents) => {
-        navigation("/contact/result", {state: {sendContents}})
-    }, [navigation])
+        axios.post(url + "/contact", sendContents).then((res) => {
+            setResponseSendContents(res.data);
+            console.log(res.data);
+            
+        })
+    }, [setResponseSendContents])
+
+    useEffect(() => {
+        if(responseSendContents!== null){
+            console.log("response");
+            console.log(responseSendContents);
+            navigation("/contact/result", {state: {responseSendContents}})
+        }
+    }, [responseSendContents, navigation])
+
     return {send};
 }
