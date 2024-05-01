170b84b00d53a2228182aac5398b5e7e0b979552
nizidara <contact@nizidara.com>
2024-05-02 08:56:52 +0900

    create comment form

--- react-app/src/components/organisms/form/CommentForm.tsx
+++ react-app/src/components/organisms/form/CommentForm.tsx
@@ -1,18 +1,46 @@
-import { FC, memo, useCallback } from "react"
-import { Button } from "react-bootstrap";
+import { ChangeEvent, FC, memo, useCallback, useEffect, useState } from "react"
+import { Button, Col, Form, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 
 export const CommentForm: FC = memo(() => {
     const navigate = useNavigate();
 
-    const onClickSend = useCallback(() => navigate("/place"), [navigate]);
+    const [comment, setComment] = useState('');
+
+    const onChangeComment = (e:ChangeEvent<HTMLInputElement>) => setComment(e.target.value);
+
+    const formData = {comment};
+
+    const onClickSend = useCallback(() => {
+        setComment('');
+        navigate("/place");
+    }, [navigate]);
     
     return (
         <>
             <hr />
-            <p>コメントフォームです</p>
-            <Button variant="secondary">写真を添付</Button> <Button variant="primary" size="lg" onClick={onClickSend}>投稿</Button><hr />
+            <Form>
+                <Form.Group className="mb-3" controlId="commentFormComment">
+                    <Form.Label>コメント</Form.Label>
+                    <Form.Control as="textarea" name="comment" value={comment} maxLength={140} onChange={onChangeComment} />
+                    <Form.Text className="text-muted">{formData.comment.length} / 140 </Form.Text>
+                </Form.Group>
+
+                <Row>
+                    <Col sm={10}>
+                        <Form.Group controlId="commentformPhoto" className="mb-3">
+                            <Form.Label>写真を添付</Form.Label>
+                            <Form.Control type="file" />
+                        </Form.Group>
+                    </Col>
+
+                    <Col sm={2}>
+                        <Button variant="primary" onClick={onClickSend}>投稿</Button>
+                    </Col>
+                </Row>
+                
+            </Form>
         </>
     )
 });
