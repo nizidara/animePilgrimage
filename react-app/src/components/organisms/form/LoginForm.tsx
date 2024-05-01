486ab29b010f19269618c3a4086d04b6c0245936
nizidara <contact@nizidara.com>
2024-05-01 15:30:07 +0900

    create loginform

--- react-app/src/components/organisms/form/LoginForm.tsx
+++ react-app/src/components/organisms/form/LoginForm.tsx
@@ -1,17 +1,41 @@
-import { FC, memo, useCallback } from "react"
-import { Button } from "react-bootstrap";
+import { ChangeEvent, FC, memo, useCallback, useState } from "react"
+import { Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { login } from "../../../type/api/user";
 
 
 export const LoginForm: FC = memo(() => {
     const navigate = useNavigate();
 
-    const onClickLogin = useCallback(() => navigate("/"), [navigate]);
+    const [loginId, setLoginId] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onChangeLoginId = (e:ChangeEvent<HTMLInputElement>) => setLoginId(e.target.value);
+    const onChangePassword = (e:ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
+
+    const login = useCallback((formData:login) => navigate("/", {state: {formData}}), [navigate]);
+    const formData = {loginId, password} as login;
+    const onClickLogin = () => login(formData);
 
     return (
         <>
             <p>ログインフォームです</p>
-            <Button variant="primary" size="lg" onClick={onClickLogin}>ログイン</Button>
+            <Form>
+                <Form.Group className="mb-3" controlId="contactFormName">
+                    <Form.Label>ログインID</Form.Label>
+                    <Form.Control value={loginId} onChange={onChangeLoginId} />
+                </Form.Group>
+                
+                <Form.Group className="mb-3" controlId="contactFormEmail">
+                    <Form.Label>パスワード</Form.Label>
+                    <Form.Control type="password" value={password} onChange={onChangePassword}/>
+                </Form.Group>
+
+                <div className="d-grid gap-2">
+                <Button variant="primary" size="lg" onClick={onClickLogin}>ログイン</Button>
+                </div>
+            </Form>
+            
         </>
     )
 });
---
+++ react-app/src/type/api/user.ts
@@ -0,0 +1,5 @@
+export type login ={
+    loginId: string;
+    password: string;
+}
+
