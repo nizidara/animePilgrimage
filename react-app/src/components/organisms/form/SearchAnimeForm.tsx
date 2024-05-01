658db4d0590639d46788dfe5adb1f98385701e73
nizidara <contact@nizidara.com>
2024-05-01 16:14:09 +0900

    create SearchForms

--- react-app/src/components/organisms/form/SearchAnimeForm.tsx
+++ react-app/src/components/organisms/form/SearchAnimeForm.tsx
@@ -1,17 +1,30 @@
-import { FC, memo, useCallback } from "react"
-import { Button } from "react-bootstrap";
+import { ChangeEvent, FC, memo, useCallback, useState } from "react"
+import { Button, Col, Form, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 
 export const SearchAnimeForm: FC = memo(() => {
     const navigate = useNavigate();
 
+    const [title, setTitle] = useState('');
+
+    const onChangeTitle = (e:ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
+
     const onClickSearch = useCallback(() => navigate("/search/anime"), [navigate]);
     
     return (
         <>
-            <p>アニメ検索フォームです</p>
-            <Button variant="secondary" size="lg" onClick={onClickSearch}>検索</Button>
+            <Form>
+                <Form.Group as={Row} className="mb-3" controlId="SearchAnimeFormTitle">
+                    <Col sm="10">
+                    <Form.Control type="search" value={title} placeholder="アニメタイトル絞り込み" onChange={onChangeTitle} />
+                    </Col>
+                    <Col sm="2">
+                    <Button variant="secondary" onClick={onClickSearch}>検索</Button>
+                    </Col>
+                </Form.Group>
+            </Form>
+            
         </>
     )
 });
--- react-app/src/components/organisms/form/SearchPlaceForm.tsx
+++ react-app/src/components/organisms/form/SearchPlaceForm.tsx
@@ -1,17 +1,43 @@
-import { FC, memo, useCallback } from "react"
-import { Button } from "react-bootstrap";
+import { ChangeEvent, FC, memo, useCallback, useState } from "react"
+import { Button, Col, Form, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 
 export const SearchPlaceForm: FC = memo(() => {
     const navigate = useNavigate();
 
+    const [name, setName] = useState('');
+    const [regionId, setRegionId] = useState('');
+
+    const onChangeName = (e:ChangeEvent<HTMLInputElement>) => setName(e.target.value);
+    const onChangeRegion = (e:ChangeEvent<HTMLSelectElement>) => setRegionId(e.target.value);
+
     const onClickSearch = useCallback(() => navigate("/search/place"), [navigate]);
     
     return (
         <>
-            <p>聖地検索フォームです</p>
-            <Button variant="secondary" size="lg" onClick={onClickSearch}>検索</Button>
+            <Form>
+                <Form.Group as={Row} className="mb-3" controlId="searchPlaceSelectRegion">
+                    <Form.Label>都道府県検索（任意）</Form.Label>
+                    <Col sm="10">
+                        <Form.Select className="mb-3" value={regionId} onChange={onChangeRegion}>
+                            <option>都道府県を選択してください</option>
+                            <option value="1">北海道</option>
+                            <option value="2">東京都</option>
+                            <option value="3">海外</option>
+                        </Form.Select>
+                    </Col>
+                </Form.Group>
+                
+                <Form.Group as={Row} className="mb-3" controlId="searchPlaceFormName">
+                    <Col sm="10">
+                        <Form.Control type="search" value={name} placeholder="聖地名検索" onChange={onChangeName} />
+                    </Col>
+                    <Col sm="2">
+                        <Button  variant="secondary" onClick={onClickSearch}>検索</Button>
+                    </Col>
+                </Form.Group>
+            </Form>
         </>
     )
 });
