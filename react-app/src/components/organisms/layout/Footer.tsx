ac3954a54d43c5769a640f280bce06c7faf8bfbe
nizidara <contact@nizidara.com>
2024-05-08 22:10:14 +0900

    fix footer, if the page can be scrolled fixed-bottom, else not

--- react-app/src/components/organisms/layout/Footer.tsx
+++ react-app/src/components/organisms/layout/Footer.tsx
@@ -1,10 +1,25 @@
-import { FC, memo } from "react"
+import { FC, memo, useEffect, useState } from "react"
 
 
 export const Footer: FC = memo(() => {
 
+    const [hasScroll, setHasScroll] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setHasScroll(window.innerHeight < document.body.offsetHeight);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        handleScroll(); // 初期設定
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <div className="mt-aoto py-2 text-center fixed-bottom">
+        <div className={`mt-auto py-2 text-center ${hasScroll ? '' : 'fixed-bottom'}`}>
             <p>Copyright&copy; にじだら, {new Date().getFullYear()} All Rights Reserved.</p>
         </div>
     )
