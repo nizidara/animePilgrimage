ece2d5f7720689ec3070f4992f041836b30f2a5a
nizidara <contact@nizidara.com>
2024-05-08 22:46:10 +0900

    create comment Card

--- react-app/src/components/organisms/card/CommentCard.tsx
+++ react-app/src/components/organisms/card/CommentCard.tsx
@@ -1,17 +1,34 @@
 import { FC, memo, useCallback } from "react"
-import { Button } from "react-bootstrap";
+import { Button, Card, ListGroup } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
+type Comment = {
+    user_name: string;
+    comment_date: Date;
+    comment: string;
+}
 
-export const CommentCard: FC = memo(() => {
+export const CommentCard: FC<Comment> = memo((props) => {
     const navigate = useNavigate();
 
     const onClickDeleteComment = useCallback(() => navigate("/delete_comment"), [navigate]);
 
+    const {user_name, comment_date, comment} = props
+
     return (
         <>
-            <b>コメントカードです</b>
-            <Button variant="secondary" size="lg" onClick={onClickDeleteComment}>削除</Button>
+            <Card>
+                <Card.Header className="d-flex justify-content-between">
+                    <div>
+                        <strong>{user_name}</strong> <small className="text-muted">{comment_date.toLocaleDateString()} {comment_date.getHours()}:{comment_date.getMinutes()}</small>
+                    </div>
+                    <Button variant="danger" className="float-right" onClick={onClickDeleteComment}>削除</Button>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Text>{comment}</Card.Text>
+                        {/* {tweet.image && <Card.Img src={tweet.image} alt="Tweet Image" />} */}
+                 </Card.Body>
+            </Card>
         </>
     )
 });
--- react-app/src/components/pages/place/PlaceDetail.tsx
+++ react-app/src/components/pages/place/PlaceDetail.tsx
@@ -1,5 +1,5 @@
 import {memo, FC, useCallback} from "react";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, ListGroup } from "react-bootstrap";
 import { DisplayMap } from "../../organisms/map/DisplayMap";
 import { PlaceSummaryCard } from "../../organisms/card/PlaceSummaryCard";
 import { PhotoCard } from "../../organisms/card/PhotoCard";
@@ -14,6 +14,15 @@ export const PlaceDetail: FC = memo(() =>{
     const onClickEdit = useCallback(() => navigate("/edit_place"), [navigate]);
     const onClickDelete = useCallback(() => navigate("/delete_place"), [navigate]);
 
+    const commentList = [
+        { comment_id: 1, comment_date: new Date(2023, 1, 2, 11, 12), user_name: 'John', comment: 'This is my first tweet!' },
+        { comment_id: 2, comment_date: new Date(2024, 3, 12, 1, 12), user_name: 'Alice', comment: 'Hello Twitter World!' },
+        { comment_id: 3, comment_date: new Date(2024, 5, 22, 11, 12), user_name: 'Bob', comment: 'I love React Bootstrap!' },
+        { comment_id: 4, comment_date: new Date(2024, 6, 1, 11, 1), user_name: 'Bob', comment: 'I love React Bootstrap!' },
+        { comment_id: 5, comment_date: new Date(2025, 1, 2, 12, 12), user_name: 'Bob', comment: 'I love React Bootstrap!' },
+        { comment_id: 6, comment_date: new Date(2025, 1, 2, 11, 12), user_name: 'Bob', comment: 'I love React Bootstrap!' },
+    ];
+
     return (
         <Container>
             <h2>聖地詳細ページです．</h2>
@@ -22,7 +31,14 @@ export const PlaceDetail: FC = memo(() =>{
             <PlaceSummaryCard />
             <PhotoCard title="リコリコ" name="すみだ水族館" />
             <CommentForm />
-            <CommentCard />
+            <ListGroup>
+                {commentList.map(comment => (
+                    <ListGroup.Item key={comment.comment_id}>
+                        <CommentCard user_name={comment.user_name} comment_date={comment.comment_date} comment={comment.comment} />
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
+            
         </Container>
     )
 });
