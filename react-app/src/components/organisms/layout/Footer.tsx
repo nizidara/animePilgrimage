7e56110ccef1e147a011c1432322ce07b47c5aa1
nizidara <contact@nizidara.com>
2024-05-11 18:31:42 +0900

    fix footer, to display always buttom contents not buttom page

--- react-app/src/components/organisms/layout/Footer.tsx
+++ react-app/src/components/organisms/layout/Footer.tsx
@@ -1,25 +1,9 @@
-import { FC, memo, useEffect, useState } from "react"
+import { FC, memo } from "react"
 
 
 export const Footer: FC = memo(() => {
-
-    const [hasScroll, setHasScroll] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            setHasScroll(window.innerHeight < document.body.offsetHeight);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        handleScroll(); // 初期設定
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
     return (
-        <div className={`mt-auto py-2 text-center ${hasScroll ? '' : 'fixed-bottom'}`}>
+        <div className="mt-auto py-2 text-center">
             <p>Copyright&copy; にじだら, {new Date().getFullYear()} All Rights Reserved.</p>
         </div>
     )
