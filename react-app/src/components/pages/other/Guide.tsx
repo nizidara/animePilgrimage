0e249beaf89820f842154be487ddb9743c96fd1e
nizidara <contact@nizidara.com>
2024-05-12 21:05:55 +0900

    fix data reference

--- react-app/src/components/pages/other/Guide.tsx
+++ react-app/src/components/pages/other/Guide.tsx
@@ -2,13 +2,10 @@ import {memo, FC} from "react";
 import { ContactForm } from "../../organisms/form/ContactForm";
 import { Container } from "react-bootstrap";
 import { FAQCard } from "../../organisms/card/FAQCard";
+import { faq } from "../../../testdatas/testdata";
 
 export const Guide: FC = memo(() =>{
-    const faq = [
-        {question: "Q1.hogehoge1", answer: "A1.fugafuga"},
-        {question: "Q2.hogehoge2", answer: "A2.fugafuga"},
-        {question: "Q3.hogehoge3", answer: "A3.fugafuga"}
-    ]
+    
     return (
         <>
             <Container>
