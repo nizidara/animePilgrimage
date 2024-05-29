8c6116fd28eb109f6d8b2c91e0e8644d41dc0e63
nizidara <contact@nizidara.com>
2024-05-29 09:14:07 +0900

    create models contact and user

--- backend/models/contact.py
+++ backend/models/contact.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, DateTime
-
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, SmallInteger
+from sqlalchemy.orm import relationship
 from database.db import Base
+import datetime
 
 """
 class Testcontact(Base):
@@ -12,15 +13,21 @@ class Testcontact(Base):
     title = Column(String(45), index=True, nullable=False)
     contents = Column(String(10000), nullable=False)
 """
-    
-class Contact(Base):
-    __tablename__ = "contacts"
 
-    contact_id = Column(Integer, primary_key=True, index=True, nullable=False)
-    contact_date = Column(DateTime, index=True, nullable=False)
-    name = Column(String(20), index=True, nullable=False)
-    email = Column(String(45), index=True, nullable=False)
-    title = Column(String(45), index=True, nullable=False)
+class Contact(Base):
+    __tablename__ = 'contacts'
+    
+    contact_id = Column(Integer, primary_key=True, autoincrement=True, comment='AUTO_INCREMENT')
+    contact_date = Column(DateTime, nullable=False, default=datetime.datetime(1970, 1, 1, 0, 0, 0))
+    name = Column(String(20), nullable=False)
+    email = Column(String(256), nullable=False)
+    title = Column(String(100), nullable=False)
     contents = Column(String(10000), nullable=False)
-    status = Column(Integer, index=True, nullable=False)
-    user_id = Column(String, index=True, nullable=True)
+    status = Column(SmallInteger, nullable=False, default=0, comment='0:not checked, 1: now checking, 2:done')
+    user_id = Column(String(32), ForeignKey('users.user_id', ondelete='SET NULL', onupdate='CASCADE'), nullable=True, comment='FK, if null is non-member')
+
+    user = relationship("User", back_populates="contacts")
+
+    __table_args__ = (
+        {'comment': 'to save contacts contents'}
+    )
---
+++ backend/models/user.py
@@ -0,0 +1,63 @@
+from sqlalchemy import Column, String, Integer, CHAR, Date, DateTime, ForeignKey, UniqueConstraint, Index, SmallInteger, ForeignKeyConstraint
+from sqlalchemy.orm import relationship
+from database.db import Base
+
+
+
+class UserAttribute(Base):
+    __tablename__ = 'user_attributes'
+    
+    user_attribute_id = Column(Integer, primary_key=True, nullable=False, default=0, comment='0 = not user, 1 = admin, 2 = normal member, 10 = special member(tmp), 99=others')
+    user_attribute_name = Column(String(20), nullable=False, default='non-member')
+    
+    users = relationship("User", back_populates="user_attribute")
+
+    __table_args__ = (
+        {'comment': 'user attribute definition table'}
+    )
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    user_id = Column(String(32), primary_key=True, nullable=False, comment='this column is generated UUID')
+    user_name = Column(String(20), nullable=False, default='no name')
+    login_id = Column(String(20), nullable=False, comment='UK, this column is used to login function.')
+    email = Column(String(256), nullable=True)
+    password = Column(CHAR(64), nullable=False, comment='this column is saved hash value which is used SHA-256')
+    error_count = Column(Integer, nullable=False, default=0, comment='this column is saved error count which is entered mistake password')
+    login_date = Column(Date, nullable=False, comment='this column is last login date or delete date')
+    flag = Column(SmallInteger, nullable=True, default=1, comment='if flag = false, this user can not use, flag = 2, this user is waiting approval, flag = 3, this user is lock due to password error count over')
+    user_attribute_id = Column(Integer, ForeignKey('user_attributes.user_attribute_id', ondelete='NO ACTION', onupdate='CASCADE'), nullable=True, comment='FK')
+
+    user_attribute = relationship("UserAttribute", back_populates="users")
+    reset_users = relationship("ResetUser", back_populates="user")
+    contacts = relationship("Contact", back_populates="user")
+
+    __table_args__ = (
+        UniqueConstraint('login_id', name='file_name_UNIQUE'),
+        Index('file_name_UNIQUE', 'login_id'),
+        {'comment': 'registered user table'}
+    )
+
+class ResetUser(Base):
+    __tablename__ = 'reset_users'
+    
+    reset_user_id = Column(String(32), primary_key=True, nullable=False, comment='this column is generated UUID')
+    user_id = Column(String(32), nullable=False, comment='FK')
+    login_id = Column(String(20), nullable=False, comment='FK')
+    email = Column(String(256), nullable=False, comment='user enter email when he forget password or login_id')
+    password = Column(CHAR(64), nullable=False, comment='this column is saved hash value tmp password which is used SHA-256')
+    issue_date = Column(DateTime, nullable=False, comment='this column is last login date or delete date')
+    flag = Column(SmallInteger, nullable=True, default=1, comment='if flag = false, this tmp password can not use, flag = true, this tmp password is waiting approval')
+
+    user = relationship("User", back_populates="reset_users")
+
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ['user_id', 'login_id'],
+            ['users.user_id', 'users.login_id'],
+            ondelete='CASCADE',
+            onupdate='CASCADE',
+        ),
+        {'comment': 'to reset password table'}
+    )
