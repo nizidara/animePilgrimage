e197f42ea912f058ae31ad2764feb7cf3b0439a3
nizidara <contact@nizidara.com>
2024-06-06 20:36:59 +0900

    create place cruds(without put(direct edit))

--- backend/cruds/place.py
+++ backend/cruds/place.py
@@ -5,10 +5,10 @@ import uuid
 import models.place as place_model
 import schemas.place as place_schema
 
-# create anime
+# create place
 async def create_place(
         db: AsyncSession, place_body: place_schema.PlaceCreate
-) -> place_model.Place:
+) -> place_schema.PlaceResponse:
     
     # convert str -> UUID
     if place_body:
@@ -25,6 +25,7 @@ async def create_place(
     db.refresh(place)
 
     # convert UUID -> str
+    response = None
     if place:
         response_dict = place.__dict__
         response_dict['place_id'] = str(uuid.UUID(bytes=place.place_id))
@@ -35,3 +36,79 @@ async def create_place(
         response = place_schema.PlaceResponse(**response_dict)
 
     return response
+
+# read list
+async def get_place_list(db:AsyncSession, name: str, anime_id: int, region_id: int) -> List[Tuple[place_schema.PlaceResponse]]:
+    # get
+    places = db.query(place_model.Place).all()
+    
+    # convert UUID -> str
+    response_list = []
+    if places:
+        for place in places:
+            response_dict = place.__dict__
+            response_dict['place_id'] = str(uuid.UUID(bytes=place.place_id))
+            if place.created_user_id is not None:
+                response_dict['created_user_id'] = str(uuid.UUID(bytes=place.created_user_id))
+            if place.edited_user_id is not None:
+                response_dict['edited_user_id'] = str(uuid.UUID(bytes=place.edited_user_id))
+            response_list.append(place_schema.PlaceResponse(**response_dict))
+
+    return response_list
+
+# read detail
+async def get_place_detail(db: AsyncSession, place_id: str) -> place_schema.PlaceResponse:
+    # convert str -> UUID
+    place_id_bytes = uuid.UUID(place_id).bytes
+
+    # get
+    place = db.query(place_model.Place).filter(place_model.Place.place_id == place_id_bytes).first()
+
+    # convert UUID -> str
+    response = None
+    if place:
+        response_dict = place.__dict__
+        response_dict['place_id'] = str(uuid.UUID(bytes=place.place_id))
+        if place.created_user_id is not None:
+            response_dict['created_user_id'] = str(uuid.UUID(bytes=place.created_user_id))
+        if place.edited_user_id is not None:
+            response_dict['edited_user_id'] = str(uuid.UUID(bytes=place.edited_user_id))
+        response = place_schema.PlaceResponse(**response_dict)
+
+    return response
+
+# update flag
+async def update_place_flag(db: AsyncSession, place_id: str, flag: int) -> place_schema.PlaceResponse:
+    # convert str -> UUID
+    place_id_bytes = uuid.UUID(place_id).bytes
+    
+    # update flag
+    place = db.query(place_model.Place).filter(place_model.Place.place_id == place_id_bytes).first()
+    response = None
+    if place:
+        place.flag = flag
+        db.commit()
+        db.refresh(place)
+
+        # convert UUID -> str
+        response_dict = place.__dict__
+        response_dict['place_id'] = str(uuid.UUID(bytes=place.place_id))
+        if place.created_user_id is not None:
+            response_dict['created_user_id'] = str(uuid.UUID(bytes=place.created_user_id))
+        if place.edited_user_id is not None:
+            response_dict['edited_user_id'] = str(uuid.UUID(bytes=place.edited_user_id))
+        response = place_schema.PlaceResponse(**response_dict)
+
+    return response
+
+# delete place
+async def delete_place(db: AsyncSession, place_id: str) -> place_model.Place:
+    # convert str -> UUID
+    place_id_bytes = uuid.UUID(place_id).bytes
+    
+    # delete
+    place = db.query(place_model.Place).filter(place_model.Place.place_id == place_id_bytes).first()
+    if place:
+        db.delete(place)
+        db.commit()
+    return place
--- backend/routers/place.py
+++ backend/routers/place.py
@@ -13,9 +13,12 @@ place_model.Base.metadata.create_all(bind=engine)
 
 # get place info detail
 @router.get("/detail/{place_id}", response_model=place_schema.PlaceResponse)
-async def place_detail(place_id: str):
-    return place_schema.PlaceResponse(place_id=place_id, anime_id=123, name="すみだ水族館", latitude=1.23, longitude=2.34, comment="さかなー", flag=1, region_id=123, created_user_id=None, edited_user_id=None)
-
+async def place_detail(place_id: str, db: AsyncSession = Depends(get_db)):
+    place = await place_crud.get_place_detail(db, place_id=place_id) 
+    if place is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return place
+ 
 # get place list by name or anime title or region
 @router.get("/list/search", response_model=List[place_schema.PlaceResponse])
 async def place_list(name: str = None, anime_id: int = None, region_id: int = None, db: AsyncSession = Depends(get_db)):
@@ -36,8 +39,11 @@ async def create_place_request(place_body: place_schema.PlaceRequestCreate):
 
 # update place.flag = 1 for display, place.flag = 0 for not display
 @router.put("/{place_id}", response_model=place_schema.PlaceResponse)
-async def update_place_flag(place_id: str, flag: int):
-    return place_schema.PlaceResponse(place_id=place_id, anime_id=123, name="すみだ水族館", latitude=1.23, longitude=2.34, comment="さかなー", flag=flag, region_id=123, created_user_id=None, edited_user_id=None)
+async def update_place_flag(place_id: str, flag: int, db: AsyncSession = Depends(get_db)):
+    place = await place_crud.update_place_flag(db, place_id=place_id, flag=flag)
+    if place is None:
+        raise HTTPException(status_code=404, detail="Anime not found")
+    return place
 
 # update place info to approve edit request
 @router.put("/edit/{place_id}", response_model=place_schema.PlaceResponse)
@@ -51,8 +57,11 @@ async def palce_edit_admin(place_id: str, place_body: place_schema.PlaceCreate):
 
 # delete anime info from DB
 @router.delete("/{place_id}")
-async def delete_place():
-    pass
+async def delete_place(place_id: str, db: AsyncSession = Depends(get_db)):
+    place = await place_crud.delete_place(db=db, place_id=place_id)
+    if place is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return {"message": "Contact deleted successfully"}
 
 # delete anime info from DB
 @router.delete("/edit/{request_place_id}")
