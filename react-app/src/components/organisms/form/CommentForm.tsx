fae5b8ae5d3ecfcea240b6b02ed1c4a4d22b3f15
nizidara <contact@nizidara.com>
2024-08-19 20:47:04 +0900

    create file upload in comment Form(not connect backend)

--- react-app/src/components/organisms/form/CommentForm.tsx
+++ react-app/src/components/organisms/form/CommentForm.tsx
@@ -1,7 +1,6 @@
 import { ChangeEvent, FC, memo, useState } from "react"
-import { Button, Col, Form, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
+import { Button, Col, Form, Image, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
 import { BsImage } from "react-icons/bs";
-import { postCommentFormData } from "../../../type/form/comment";
 import { usePostComment } from "../../../hooks/comments/usePostComment";
 
 type commentFormData = {
@@ -14,14 +13,25 @@ export const CommentForm: FC<commentFormData> = memo((props) => {
     const {post} = usePostComment();
 
     const [comment, setComment] = useState('');
+    const [selectedImages, setSelectedImages] = useState<File[]>([]);
 
     const onChangeComment = (e:ChangeEvent<HTMLInputElement>) => setComment(e.target.value);
+    const onImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && selectedImages.length + e.target.files.length <= 4) {
+            setSelectedImages([...selectedImages, ...Array.from(e.target.files)]);
+        }
+    };
 
-    const formData = {comment} as postCommentFormData
+    const removeImage = (index: number) => {
+        const updatedImages = [...selectedImages];
+        updatedImages.splice(index, 1);
+        setSelectedImages(updatedImages);
+    };
 
     const onClickSend = () => {
-        post(formData, placeId, onCommentPosted);
+        post(comment, placeId, selectedImages, onCommentPosted);
         setComment('');
+        setSelectedImages([]);
       };
     
     return (
@@ -32,7 +42,7 @@ export const CommentForm: FC<commentFormData> = memo((props) => {
                     <Form.Label>コメント</Form.Label>
                     <Form.Control as="textarea" name="comment" value={comment} maxLength={140} onChange={onChangeComment} />
                     <Form.Text className="text-muted d-flex justify-content-end align-items-center">
-                        {formData.comment.length} / 140
+                        {comment.length} / 140
                     </Form.Text>
                 </Form.Group>
 
@@ -44,12 +54,20 @@ export const CommentForm: FC<commentFormData> = memo((props) => {
                                     <a className="img"><BsImage /></a>
                                 </OverlayTrigger>
                             </Form.Label>
-                            <Form.Control type="file" hidden/>
+                            <Form.Control type="file" accept="image/*" multiple hidden onChange={onImageChange} />
                         </Form.Group>
+                        <div className="d-flex flex-wrap">
+                            {selectedImages.map((image, index) => (
+                                <div key={index} className="position-relative m-1">
+                                    <Image src={URL.createObjectURL(image)} thumbnail width={200} height={200} />
+                                    <Button variant="danger" size="sm" className="position-absolute top-0 end-0" onClick={() => removeImage(index)}>×</Button>
+                                </div>
+                            ))}
+                        </div>
                     </Col>
 
                     <Col xs="auto" className="d-flex justify-content-end align-items-center">
-                        <Button variant="primary" disabled={!formData.comment} onClick={onClickSend} >投稿</Button>
+                        <Button variant="primary" disabled={!comment} onClick={onClickSend} >投稿</Button>
                     </Col>
                 </Row>
                 
