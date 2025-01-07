2823d01384d346dea65190a127f220c09e594a71
nizidara <contact@nizidara.com>
2025-01-07 10:27:20 +0900

    fix send email contents

--- backend/logic/mail.py
+++ backend/logic/mail.py
@@ -7,15 +7,35 @@ from properties.properties import sender_email, sender_password, recipient_email
 
 
 # メール送信処理
-def send_email(contact_data: contact_schema.ContactCreate):
+def notify_new_contact(contact_data: contact_schema.ContactCreate):
     message = MIMEMultipart()
     message["From"] = sender_email
     message["To"] = recipient_email
-    message["Subject"] = "新しいお問い合わせ"
+    message["Subject"] = contact_data.title
 
     # メール本文の作成
     body = f"名前: {contact_data.name}\nメールアドレス: {contact_data.email}\nメッセージ:\n{contact_data.contents}"
     message.attach(MIMEText(body, "plain"))
+
+    # SMTPサーバーに接続してメールを送信
+    try:
+        with smtplib.SMTP(email_server, 587) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, recipient_email, message.as_string())
+    except Exception as e:
+        print(f"メール送信中にエラーが発生しました: {e}")
+
+def send_complete_contact(contact_data: contact_schema.ContactCreate):
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = contact_data.email
+    message["Subject"] = "お問い合わせが完了しました"
+
+    # メール本文の作成
+    body = f"名前: {contact_data.name}さん\nメールアドレス: {contact_data.email}\n題名: {contact_data.title}\n\nメッセージ:\n{contact_data.contents}"
+    footer = f"\n\nにじげんたび\nMAIL: {sender_email}"
+    message.attach(MIMEText(body + footer, "plain"))
 
     # SMTPサーバーに接続してメールを送信
     try:
