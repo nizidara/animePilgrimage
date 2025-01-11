7ab3b34aeab31974c5625ebe80822dee4a32980d
nizidara <contact@nizidara.com>
2025-01-11 15:36:21 +0900

    fix to get flag = 1 only or if get all flag auth is neccessary

--- react-app/src/hooks/anime/useGetAnimeDetail.ts
+++ react-app/src/hooks/anime/useGetAnimeDetail.ts
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from "react"
 import axios from "axios";
 import { fastAPIURL } from "../../properties/properties";
 import { responseAnimeData } from "../../type/api/anime";
+import api from "../../api/axiosInstance";
 
 export const useGetAnimeDetail = (anime_id: string | number | null) => {
     const [anime, setAnime] = useState<responseAnimeData>();
@@ -10,15 +11,27 @@ export const useGetAnimeDetail = (anime_id: string | number | null) => {
 
     const fetchAnimeDetail = useCallback(() => {
         if(anime_id){
+            //not auth
             axios.get(`${fastAPIURL}/anime/detail/${anime_id}`)
-        .then(response => {
-            setAnime(response.data);
-            setLoading(false);
-        })
-        .catch(() => {
-            setError("アニメ情報取得中にエラーが発生しました");
-            setLoading(false);
-        });
+            .then(response => {
+                setAnime(response.data);
+            }).catch((err) =>{
+                //anime.flag == 1
+                if(err.response.status === 401){
+                    api.get(`${fastAPIURL}/anime/detail/${anime_id}`)
+                    .then(response => {
+                        setAnime(response.data);
+                    })
+                    .catch(() => {
+                        setError("この作品は現在は利用できません。");
+                    })
+                }else{
+                    setError("アニメ情報取得中にエラーが発生しました");
+                }
+            })
+            .finally(() =>{
+                setLoading(false);
+            })
         }
     }, [anime_id]);
 
