edc85b9936505a9f8234f2e31fba1ff588aebccc
nizidara <contact@nizidara.com>
2025-01-31 11:35:46 +0900

    fix to connect fastAPI Render URL

--- react-app/src/properties/properties.ts
+++ react-app/src/properties/properties.ts
@@ -1,4 +1,5 @@
-export const fastAPIURL = "http://127.0.0.1:8000"
+// export const fastAPIURL = "http://127.0.0.1:8000"
+export const fastAPIURL = "https://animepilgrimage.onrender.com"
 export const mapboxAccessToken = 'pk.eyJ1Ijoibml6aWRhcmEiLCJhIjoiY2x0Y3k0dmphMDBxbzJqbzRkb2oycjU4aSJ9.pLwmNt5sNVoED0oWCjp_6Q'
 export const devMapboxAccessToken = 'pk.eyJ1Ijoic2VhcmNoLW1hY2hpbmUtdXNlci0xIiwiYSI6ImNrb2E2YzZheDA2ZHoydnFxOG95cjNuMWoifQ.TXC7Q4hjr1nktFft_n0Eog' //dev mapbox token
 export const mapboxFlag = 1 //0: not connect mapbox, 1:connect mapbox
