74f8155e84865ef5ea299edb67d3d4797eeb7e65
nizidara <contact@nizidara.com>
2025-02-01 17:09:53 +0900

    fix properties

--- backend/properties/properties.py
+++ backend/properties/properties.py
@@ -17,9 +17,11 @@ database = os.getenv('DB_NAME')
 
 DB_URL = f"mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8mb4"
 
-#frontend setting(dev)
+#frontend setting
 origins = [
     "http://localhost:3000",
+    "https://pilgrimage.nizidara.com",
+    "https://anime-pilgrimage-delta.vercel.app",
 ]
 
 # image setting(local)
