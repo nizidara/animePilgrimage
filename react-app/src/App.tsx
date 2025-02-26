89ae88a026c1221cb898652b360ed83225bb2991
nizidara <contact@nizidara.com>
2025-02-26 23:09:19 +0900

    fix meta tag

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,10 +4,30 @@ import { Router } from './router/Router';
 import { AuthProvider } from './providers/AuthContext';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
+const defaultStructData = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "name": "にじげんたび",
+    "url": "https://pilgrimage.nizidara.com",
+    "description": "アニメ系聖地巡礼向けのWebアプリ",
+    "thumbnailUrl": "https://pilgrimage.nizidara.com/logo512.png",
+    "publisher": {
+        "@type": "Organization",
+        "name": "にじげんたび"
+    }
+};
+
 function App() {
     return (
         <HelmetProvider>
-            <Helmet defaultTitle="にじげんたび" titleTemplate="%s - にじげんたび" />
+            <Helmet defaultTitle="にじげんたび" titleTemplate="%s - にじげんたび">
+                <meta name="description" content="アニメ系聖地巡礼向けWebアプリ『にじげんたび』" />
+                <meta property="og:image" content="https://pilgrimage.nizidara.com/logo512.png" />
+                <meta name="twitter:image" content="https://pilgrimage.nizidara.com/logo512.png" />
+                <script type="application/ld+json">
+                    {JSON.stringify(defaultStructData)}
+                </script>
+            </Helmet>
             <AuthProvider>
                 <Router />
             </AuthProvider>
